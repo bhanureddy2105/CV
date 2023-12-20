9e3da6118f185e78839243c4462edfc9d893d368
Vangeti Bhanu Prakash Reddy <60293758+bhanureddy2105@users.noreply.github.com>
2023-12-20 05:06:30 -0600

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link, HashRouter } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import Projects from './components/Projects';
 import { HomePage } from './components/HomePage';
@@ -7,14 +7,14 @@ import { HomePage } from './components/HomePage';
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Layout> <HomePage/> </Layout>} />
           <Route path="/home" element={<Layout> <HomePage/> </Layout>} />
           <Route path="/projects" element={<Layout> <Projects/> </Layout>} />
           <Route path="*" element={<div>Not Found</div>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
 
   );
