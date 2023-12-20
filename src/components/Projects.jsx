d0b41fa36af8725705ca93899e6c26c9fcd64cdd
Vangeti Bhanu Prakash Reddy <60293758+bhanureddy2105@users.noreply.github.com>
2023-12-20 04:37:38 -0600

    projects added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,48 +1,44 @@
 import { Card } from './Card'
 import { SimpleLayout } from './SimpleLayout'
-import logoAnimaginary from '../images/logos/animaginary.svg'
-import logoCosmos from '../images/logos/cosmos.svg'
-import logoHelioStream from '../images/logos/helio-stream.svg'
-import logoOpenShuttle from '../images/logos/open-shuttle.svg'
-import logoPlanetaria from '../images/logos/planetaria.svg'
+import {
+  GitHubIcon,
+} from './SocialIcons'
+import { useEffect, useState } from 'react'
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../firebase';
 
-const projects = [
-  {
-    name: 'Planetaria',
-    description:
-      'Creating technology to empower civilians to explore space on their own terms.',
-    link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
-    logo: logoPlanetaria,
-  },
-  {
-    name: 'Animaginary',
-    description:
-      'High performance web animation library, hand-written in optimized WASM.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoAnimaginary,
-  },
-  {
-    name: 'HelioStream',
-    description:
-      'Real-time video streaming library, optimized for interstellar transmission.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoHelioStream,
-  },
-  {
-    name: 'cosmOS',
-    description:
-      'The operating system that powers our Planetaria space shuttles.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoCosmos,
-  },
-  {
-    name: 'OpenShuttle',
-    description:
-      'The schematics for the first rocket I designed that successfully made it to orbit.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoOpenShuttle,
-  },
-]
+// const projects = [
+//   {
+//     name: 'Planetaria',
+//     description:
+//       'Creating technology to empower civilians to explore space on their own terms.',
+//     link: { href: 'http://planetaria.tech' },
+//   },
+//   {
+//     name: 'Animaginary',
+//     description:
+//       'High performance web animation library, hand-written in optimized WASM.',
+//     link: { href: '#' },
+//   },
+//   {
+//     name: 'HelioStream',
+//     description:
+//       'Real-time video streaming library, optimized for interstellar transmission.',
+//     link: { href: '#' },
+//   },
+//   {
+//     name: 'cosmOS',
+//     description:
+//       'The operating system that powers our Planetaria space shuttles.',
+//     link: { href: '#' },
+//   },
+//   {
+//     name: 'OpenShuttle',
+//     description:
+//       'The schematics for the first rocket I designed that successfully made it to orbit.',
+//     link: { href: '#' },
+//   },
+// ]
 
 function LinkIcon(props) {
   return (
@@ -60,10 +56,40 @@ export const metadata = {
   description: 'Things I’ve made trying to put my dent in the universe.',
 }
 
+function SocialLink({ icon: Icon, ...props }) {
+  return (
+    <a className="group -m-1 p-1" {...props}>
+      <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+    </a>
+  )
+}
+
 export default function Projects() {
+
+  const getProjects = async () =>{
+    await getDocs(collection(db, 'projects'))
+              .then(querySnapshot => {
+                  const data = querySnapshot.docs.map(doc => {
+                      return {
+                          ...doc.data(),
+                          id: doc.id
+                      }
+                  })
+                  setProjects(data)
+              })
+  }
+  
+  useEffect(()=>{
+    getProjects()
+  },[])
+  
+  const [projects,setProjects] = useState([])
+
+
+
   return (
     <SimpleLayout
-      title="Things I’ve made trying to put my dent in the universe."
+      title="Embarking on My Digital Odyssey: Showcasing Projects That Left a Mark on the Code Canvas"
       intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
     >
       <ul
@@ -73,20 +99,20 @@ export default function Projects() {
         {projects.map((project) => (
           <Card as="li" key={project.name}>
             <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-              <img
-                src={project.logo}
-                alt=""
-                className="h-8 w-8"
-                unoptimized
+              <SocialLink
+                href={project.link.href}
+                target="_blank"
+                aria-label="Follow on GitHub"
+                icon={GitHubIcon}
               />
             </div>
             <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-              <Card.Link href={project.link.href}>{project.name}</Card.Link>
+              <Card.Link href={project.link.href} target="_blank">{project.name}</Card.Link>
             </h2>
             <Card.Description>{project.description}</Card.Description>
             <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
               <LinkIcon className="h-6 w-6 flex-none" />
-              <span className="ml-2">{project.link.label}</span>
+              <a href={project.link.href} target="_blank"><span className="ml-2">{project.link.label || 'github.com'}</span></a>
             </p>
           </Card>
         ))}
