0ce79b6b9f1d3a430a97309b8a9c1fb31a502a9c
Vangeti Bhanu Prakash Reddy <60293758+bhanureddy2105@users.noreply.github.com>
2023-12-20 06:14:18 -0600

    minor changes

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -17,7 +17,7 @@ function Resume() {
     };
 
     return (       
-        <a href="/resume/resume.pdf" download="Resume.pdf">
+        <a href="https://drive.google.com/file/d/18-ZIHHiWyfdPOMxR7pVii-EAL7vymBSd/view?usp=sharing" target="_blank" rel="noopener noreferrer" download="Resume.pdf">
             <Button variant="secondary" className="group mt-6 w-full">
                 Download Resume
                 <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
