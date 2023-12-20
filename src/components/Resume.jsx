53451306d838bff64b325e130d29357e752fefa6
Vangeti Bhanu Prakash Reddy <60293758+bhanureddy2105@users.noreply.github.com>
2023-12-20 05:46:01 -0600

    minor change

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -17,10 +17,12 @@ function Resume() {
     };
 
     return (       
-        <Button onClick={handleDownload} variant="secondary" className="group mt-6 w-full">
-            Download Resume
-            <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
-        </Button>
+        <a href="/Resume.pdf" download>
+            <Button variant="secondary" className="group mt-6 w-full">
+                Download Resume
+                <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
+            </Button>
+        </a>
     )
 }
 
